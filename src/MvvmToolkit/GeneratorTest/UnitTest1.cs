21d4759592ab9fff014e6bad577e2576ece2d8e3
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2025-01-21 14:56:36 +0900

    bug : CodeGenerator 제작하는데 참조시 오류 발생

--- src/MvvmToolkit/GeneratorTest/UnitTest1.cs
+++ src/MvvmToolkit/GeneratorTest/UnitTest1.cs
@@ -1,29 +1,29 @@
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis;
-using Xunit;
-using MvvmToolkit.Core.Attributes;
+//using Microsoft.CodeAnalysis.CSharp;
+//using Microsoft.CodeAnalysis;
+//using Xunit;
+//using MvvmToolkit.Core.Attributes;
 
-namespace GeneratorTest
-{
-    public partial class PropertyGenerator
-    {
-        [Property]
-        private string _name;
-        [Fact]
-        public Task GeneratesEnumExtensionsCorrectly()
-        {
-            // 테스트 할 소스코드
-            var source = @"
-namespace GeneratorTest;
-public partial class PropertyGenerator
-{
-[Property]
-private string _name;
-}";
+//namespace GeneratorTest
+//{
+//    public partial class PropertyGenerator
+//    {
+//        [Property]
+//        private string _name;
+//        [Fact]
+//        public Task GeneratesEnumExtensionsCorrectly()
+//        {
+//            // 테스트 할 소스코드
+//            var source = @"
+//namespace GeneratorTest;
+//public partial class PropertyGenerator
+//{
+//[Property]
+//private string _name;
+//}";
 
-            // 소스 코드를 도우미에 전달하고 스냅샷 테스트 출력
-            return TestHelper.Verify(source);
-        }
-    }    
+//            // 소스 코드를 도우미에 전달하고 스냅샷 테스트 출력
+//            return TestHelper.Verify(source);
+//        }
+//    }    
      
-}
+//}
---
+++ src/MvvmToolkit/WpfCore.Test/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MvvmToolkit.Core.Components;
+
+namespace WpfCore.Test
+{
+    public partial class MainViewModel : ViewModelBase
+    {
+        [Property]
+        private string _name;
+        [RelayCommand]
+        private void OnClick()
+        {
+            Console.WriteLine("Click");
+        }
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
