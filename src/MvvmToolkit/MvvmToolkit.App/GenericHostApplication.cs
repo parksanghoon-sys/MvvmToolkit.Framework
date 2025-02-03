014e948ba20b07f00ac026018cec049047c480d3
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2025-02-03 13:22:28 +0900

    feat : 비정상 종료시 dump 파일 생성하도록 구현

--- src/MvvmToolkit/MvvmToolkit.App/GenericHostApplication.cs
+++ src/MvvmToolkit/MvvmToolkit.App/GenericHostApplication.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using MvvmToolkit.App.Helper;
 using System;
 using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace MvvmToolkit.App
 {
     public abstract class GenericHostApplication : Application
     {
         private static IHost? Host { get; set; }
+        private ILogger<GenericHostApplication>? _logger;
         private bool _canGenerateDump;
         private CoreDumpHelper.MiniDumpType _dumpType = CoreDumpHelper.MiniDumpType.MiniDumpNormal;
         /// <summary>
@@ -26,6 +29,65 @@ namespace MvvmToolkit.App
 
             return $"{dirPath}/[{exeName}]{dateTime}.dmp";
         }
+        /// <summary>
+        /// Crash 발생 시 Dump를 생성할 수 있는 옵션을 설정합니다.
+        /// </summary>
+        /// <param name="canGenerateDump">덤프를 생성할지 여부.</param>
+        /// <param name="dumpType">생성할 덤프의 타입</param>
+        protected void SetDumpOption(bool canGenerateDump, CoreDumpHelper.MiniDumpType dumpType = CoreDumpHelper.MiniDumpType.MiniDumpNormal)
+        {
+            _canGenerateDump = canGenerateDump;
+            _dumpType = dumpType;
+        }
+        protected GenericHostApplication()
+        {
+            Dispatcher.UnhandledException += Dispatcher_UnhandledException;
+            Dispatcher.UnhandledExceptionFilter += Dispatcher_UnhandledExceptionFilter;
 
+            ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
+        /// <summary>
+        /// Dispatchers the unhandled exception filter using the specified sender.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private void Dispatcher_UnhandledExceptionFilter(object sender, DispatcherUnhandledExceptionFilterEventArgs e)
+        {
+            try
+            {
+                e.RequestCatch = true;
+
+                _logger?.LogError(e.Exception, $"[App Error Catch] {e.Exception}");
+                _logger?.LogError(e.Exception, $"[App_DispatcherUnhandledExceptionFilter] {e.Exception.Message}");
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+        /// <summary>
+        /// Dispatchers the unhandled exception using the specified sender.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private void Dispatcher_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                e.Handled = true;
+
+                _logger?.LogError(e.Exception, $"[App Error Catch] {e.Exception}");
+                _logger?.LogError(e.Exception, $"[App_DispatcherUnhandledException] {e.Exception.Message}");
+                if (_canGenerateDump)
+                {
+                    string dumpPath = GetDumpPath();
+                    CoreDumpHelper.CreateMemoryDump(_dumpType, dumpPath);
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
     }
 }
